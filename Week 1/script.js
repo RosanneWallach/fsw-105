43e7c8e327be2b931654750072245e88d6bf71b3
RosanneWallach <66342005+RosanneWallach@users.noreply.github.com>
2020-08-02 02:00:39 -0400

    Add files via upload

---
+++ Week 1/script.js
@@ -0,0 +1,9 @@
+var person = {
+    firstName: "Rosanne",
+    lastName : "Wallach",
+    id       : 1111,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  
