6494b6f1244cc1187f7b2c9101a490b097d5709c
RosanneWallach <66342005+RosanneWallach@users.noreply.github.com>
2020-08-12 12:03:50 -0400

    Add files via upload

---
+++ Olympics/index.js
@@ -0,0 +1,28 @@
+if (5 > 3){
+console.log("is greater than")
+}
+var x= "cat"
+if(x.length===3){
+    console.log("is the length")
+}
+
+
+if(x==="dog"){
+    console.log("should not equal")
+} else{ console.log("not the same")
+
+}
+var person = {
+    name: "Bobby",
+    age: 12
+}
+if(person.age >=18){
+    console.log (`${person.name} is allowed to go to the movies`)
+} else{ console.log(`${person.name} is not allowed to go to the movies`)
+
+}
+console.log (person.age)
+
+if (person.name.charAt(0)==="B"){
+console.log (`${person.name} is allowed to go to the movies`)
+} else{ console.log(`${person.name} is not allowed to go to the movies`)}
