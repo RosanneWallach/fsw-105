802dfabf13a578c63b7c0e5b1d6390ac3e3511b5
RosanneWallach <66342005+RosanneWallach@users.noreply.github.com>
2020-08-16 00:19:05 -0400

    Add files via upload

---
+++ Week 3/index.js
@@ -0,0 +1,37 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+var count = 0;
+
+for (var i = 0; i < officeItems.length; i++){
+   if (officeItems[i] === "computer"){
+        count++;
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+   {
+      name: "Mike"
+      age: 12,
+      gender: "male"
+   },{
+      name: "Madeline"
+      age: 80
+      gender: "female"
+   },{
+      name: "Cheryl"
+      age: 22,
+      gender: "female"
+   },{
+      name: "Sam"
+      age: 30,
+      gender: "male"
+   },{
+      name: "Suzy"
+      age: 4,
+      gender: "female"
+   }
+]
+
+if(person.age >=18){
+   console.log (`${person.name} is allowed to go to the movies`)
+} else{ console.log(`${person.name} is not allowed to go to the movies`)
+
+}
+console.log (person.age)
