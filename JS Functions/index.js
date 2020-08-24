f0e62a797b488537e598644536a1fc7f9aae7af4
RosanneWallach <66342005+RosanneWallach@users.noreply.github.com>
2020-08-24 02:52:24 -0400

    Add files via upload

---
+++ JS Functions/index.js
@@ -0,0 +1,12 @@
+function sum(num1, num2){
+    return num1 + num2
+}
+
+var result = sum(10,25)
+console.log(result)
+
+function sum(num1, num2, num3){
+    return num1 + num2 + num3
+}
+var result =sum(10,25,45)
+console.log(result)
