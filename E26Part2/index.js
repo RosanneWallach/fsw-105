5524a6519a40372658578a5544caa0a5713df6b3
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2020-09-01 23:41:23 -0400

    Add files via upload

---
+++ E26Part2/index.js
@@ -0,0 +1,19 @@
+var name = "John"
+var age = 101
+
+function runForLoop(pets) [
+    var petObjects = []
+    for (var i = 0; i < pets.length; i++) {
+
+        var pet = { type: pets[i]}
+        name;
+        if (pets[i] === "cat") [
+            name = "fluffy"
+        ] else {
+            name = "spot"
+        }
+        console.log("pet name: ", name)
+        pet.name = namepetObkects.push(pet)
+    
+]
+runForLoop(["cat", "dog"])
