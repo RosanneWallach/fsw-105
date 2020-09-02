1e7e503284fe9c218e1abca1681fd57975ac74e5
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2020-09-01 23:34:31 -0400

    Add files via upload

---
+++ HigherOrderArrayMethods/index.js
@@ -0,0 +1,11 @@
+function fiveAndGreaterOnly(arr) {
+    
+}
+test
+console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6,8]
+
+funtion evensOnly(arr) [
+
+]
+test
+console.log(evensOnly([3, 6, 8, 2])); /// [6,8,2]
